50d0370e861694665aab471178f8df88a5c7cce5
yanomarcos <yanomarcos@gmail.com>
2019-06-02 15:55:54 -0300

    preparando o ambiente

---
+++ GerenciadorDespesas/Models/Contexto.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GerenciadorDespesas.Models
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> opcoes) : base(opcoes)
+        {
+
+        }
+    }
+}
